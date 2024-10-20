82da8379e8e360bcb298f017c5d2413ce0ac2ffa
ModernDesigners <avtojikuridze@gmail.com>
2024-10-20 20:40:43 +0400

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className="text-center text-4xl font-bold ">
+      Digital Industry !! New Era
+    </h1>
+    </>
+  )
+}
+
+export default App
